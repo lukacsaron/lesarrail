454885e4539422e5f387e020c8310d1848ba94c9
Aron Lukacs <aron.lukacs@flashbirdcreative.com>
2016-04-16 13:24:32 +0100

    amends

---
+++ wp-content/themes/lesarrail/page-templates/page-testimonials.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * Template Name: Testimonials Page
+ *
+ * Template for displaying a page without sidebar even if a sidebar widget is published
+ *
+ * @package understrap
+ */
+
+get_header(); ?>
+
+<div class="wrapper" id="page-wrapper">
+    
+
+    <?php $attached_images = get_post_meta($post->ID, "wpcf-gallery-item", false);
+        if ($attached_images[0]=="") { ?>
+
+        <!-- If there are no custom fields, show nothing -->
+
+        <?php } else { ?>
+    
+    <div id="slideshow">
+       <?php foreach($attached_images as $attached_images) {
+            echo '<div class="slideshow-item" style="background-image:url('.$attached_images.');"></div>';
+            } ?>
+    </div>
+
+        <?php } ?>
+    
+    
+    <div  id="content" class="container">
+        <div class="row">
+            <div class="col-md-12">
+        
+           <div id="primary" class="col-md-6 content-area">
+
+                <main id="main" class="site-main" role="main">
+
+                    <?php $args = array( 'post_type' => 'testimonial', 'posts_per_page' => 30 );
+              $loop = new WP_Query( $args );
+        
+            while ( $loop->have_posts() ) : $loop->the_post(); ?> 
+        
+                                
+                        
+                        <article id="post-<?php the_ID(); ?>" <?php post_class('closed'); ?>>
+
+                            <header class="entry-header">
+                                <?php the_title( '<h5 class="entry-title">', '</h5>' ); ?>
+                            </header><!-- .entry-header -->
+
+                            <div class="entry-content">
+                                <?php the_content(); ?>
+                            </div><!-- .entry-content -->
+
+                            <footer class="entry-footer">
+                                <?php understrap_entry_footer(); ?>
+                            </footer><!-- .entry-footer -->
+                            <div>
+            
+                            </div>
+                        </article><!-- #post-## -->
+                        <hr>
+                                
+                                
+                                       
+				<?php endwhile; ?>
+
+                </main><!-- #main -->
+
+            </div><!-- #primary -->
+                </div>
+        </div>
+        
+    </div><!-- Container end -->
+    
+</div><!-- Wrapper end -->
+
+<?php get_footer(); ?>
