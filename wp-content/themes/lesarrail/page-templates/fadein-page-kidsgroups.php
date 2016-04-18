6e8d65adbe5dc1d19c44e80388878389865f4165
Aron Lukacs <aron.lukacs@flashbirdcreative.com>
2016-04-19 00:14:52 +0200

    new page

---
+++ wp-content/themes/lesarrail/page-templates/fadein-page-kidsgroups.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * Template Name: Dual Content Page
+ *
+ * Template for displaying a page without sidebar even if a sidebar widget is published
+ *
+ * @package understrap
+ */
+
+get_header(); ?>
+
+<div class="wrapper" id="page-wrapper">
+    
+
+    <?php $attached_images = get_post_meta($post->ID, "wpcf-gallery-item", false);
+        if ($attached_images[0]=="") { ?>
+
+        <!-- If there are no custom fields, show nothing -->
+
+        <?php } else { ?>
+     
+    <div id="slideshow">
+       <?php foreach($attached_images as $attached_images) {
+            echo '<div class="slideshow-item" style="background-image:url('.$attached_images.');"></div>';
+            } ?>
+    </div>
+
+        <?php } ?>
+    
+    
+    <div  id="content" class="container">
+        <div class="row">
+            <div class="col-md-12">
+        
+               <div id="primary" class="col-md-6 content-area">
+
+                    <main id="main" class="site-main" role="main">
+
+                        <?php while ( have_posts() ) : the_post(); ?>
+
+                            <?php get_template_part( 'loop-templates/content', 'page' ); ?>
+                            <?php   $second_title = get_post_meta($post->ID, "wpcf-second-title", false);
+                                    $second_content = get_post_meta($post->ID, "wpcf-second-content", false);
+                            ?>
+
+                            <?php
+                                // If comments are open or we have at least one comment, load up the comment template
+                                if ( comments_open() || get_comments_number() ) :
+
+                                    comments_template();
+
+                                endif;
+                            ?>
+
+                        
+
+                    </main><!-- #main -->
+
+                </div><!-- #primary -->
+                <div id="secondary">
+                    <h2><?php echo $second_title; ?></h2>
+                    <div>
+                       <?php echo $second_content; ?> 
+                    </div>
+                </div>
+                
+               <?php endwhile; // end of the loop. ?> 
+            </div>
+        </div>
+        
+    </div><!-- Container end -->
+    
+</div><!-- Wrapper end -->
+
+<?php get_footer(); ?>
