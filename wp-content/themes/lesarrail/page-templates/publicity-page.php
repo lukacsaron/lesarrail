b6f168187b6af578f26aad89293a8315ac28efd4
Aron Lukacs <aron.lukacs@flashbirdcreative.com>
2016-04-20 09:06:29 +0200

    publi

--- wp-content/themes/lesarrail/page-templates/publicity-page.php
+++ wp-content/themes/lesarrail/page-templates/publicity-page.php
@@ -11,48 +11,50 @@ get_header(); ?>
 
 <div class="wrapper" id="page-wrapper">
     
+
+    <?php $attached_images = get_post_meta($post->ID, "wpcf-gallery-item", false);
+        if ($attached_images[0]=="") { ?>
+
+        <!-- If there are no custom fields, show nothing -->
+
+        <?php } else { ?>
+     
+    <div id="slideshow">
+       <?php foreach($attached_images as $attached_images) {
+            echo '<div class="slideshow-item" style="background-image:url('.$attached_images.');"></div>';
+            } ?>
+    </div>
+
+        <?php } ?>
+    
+    
     <div  id="content" class="container">
         <div class="row">
             <div class="col-md-12">
         
-           <div id="primary" class="col-md-12 content-area">
+           <div id="primary" class="col-md-6 content-area">
+
                 <main id="main" class="site-main" role="main">
-                    <div class="col-md-6">
-                    <?php $args = array( 'post_type' => 'testimonial', 'posts_per_page' => 30 );
-              $loop = new WP_Query( $args );
-              $count = 0;
-            while ( $loop->have_posts() ) : $loop->the_post(); $count ++; ?> 
-        
-                                
-                        
-                        <article id="post-<?php the_ID(); ?>" <?php post_class('closed'); ?>>
 
-                            <div class="entry-content">
-                                <?php the_content(); ?>
-                                <?php the_title( '<a class="entry-title">', '</a>' ); ?>
-                            </div><!-- .entry-content -->
+                    <?php while ( have_posts() ) : the_post(); ?>
 
-                            <footer class="entry-footer">
-                                <?php understrap_entry_footer(); ?>
-                            </footer><!-- .entry-footer -->
-                            <div>
-            
-                            </div>
-                        
-                        </article><!-- #post-## -->
-                        
-                    <?php if( $count == 12 ) {
-                        echo '</div><div class="col-md-6">';
-                    } continue; ?>
-                                
-                                
-                                       
-				<?php endwhile; ?>
-                    </div>
+                        <?php get_template_part( 'loop-templates/content', 'page' ); ?>
+
+                        <?php
+                            // If comments are open or we have at least one comment, load up the comment template
+                            if ( comments_open() || get_comments_number() ) :
+
+                                comments_template();
+
+                            endif;
+                        ?>
+
+                    <?php endwhile; // end of the loop. ?>
+
                 </main><!-- #main -->
 
             </div><!-- #primary -->
-            </div>
+                </div>
         </div>
         
     </div><!-- Container end -->
